aa2fc697acf751127f0886b91573cebd31873437
Alma Cleto <emscleto11@gmail.com>
2023-02-20 10:19:02 +0800

    first commit

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return response()->json($posts);
+    }
+
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->description = $request->input('description');
+        $post->save();
+
+        return response()->json(['message' => 'Post created successfully', 'post' => $post]);
+    }
+
+    public function show(Post $post)
+    {
+        return response()->json($post);
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return response()->json(['message' => 'Post deleted successfully']);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Middleware\AdminMiddleware;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/admin', [AdminController::class, 'index'])->middleware(AdminMiddleware::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::delete('posts/{post}', [PostController::class, 'destroy']);
